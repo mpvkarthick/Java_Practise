bc40d1afd14cbe70c7f99facee309ea6e537f04a
Karthik Mannepalli <mpvkarthick@gmail.com>
2020-07-25 20:10:51 -0700

    Create LongestFilePath_388.java

---
+++ src/main/java/com/example/demo/datastructures/LongestFilePath_388.java
@@ -0,0 +1,35 @@
+/**
+ * @author Karthik Mannepalli 
+ * Problem Statement : 
+ * Level : 
+ */
+package com.example.demo.datastructures;
+
+/**
+ * @author Karthik Mannepalli 
+ * @Problem-Statement : https://leetcode.com/problems/longest-absolute-file-path/
+ * @Level : Medium 
+ *
+ */
+public class LongestFilePath_388 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	 /**
+	 * @param input
+	 * @return
+	 */
+	public int lengthLongestPath(String input) {
+	        input = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+	        
+	        
+	        return 5;
+	    }
+
+}
